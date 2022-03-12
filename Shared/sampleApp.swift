5bfb93cb9ad8a9929f04a169436b1851e0b67691
author <author@example.com>
2022-03-12 20:43:30 +0800

    Initial Commit

---
+++ Shared/sampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  sampleApp.swift
+//  Shared
+//
+//  Created by admin on 2022/03/12.
+//
+
+import SwiftUI
+
+@main
+struct sampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
